96e54303cbdbafc17904af9e0d6e0ca67b391bfc
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-04-13 18:31:48 -0300

    Alternativa con guardas

--- src/Library.hs
+++ src/Library.hs
@@ -12,11 +12,14 @@ pesoBase altura = 3 * metrosACm (longitudDeLaBase altura)
 pesoCopa :: Number -> Number
 pesoCopa altura = 2 * metrosACm (longitudDeLaCopa altura)
 
+longitudMaximaDeBase :: Number
+longitudMaximaDeBase = 3
+
 longitudDeLaBase :: Number -> Number
-longitudDeLaBase altura = min 3 altura
+longitudDeLaBase altura = min longitudMaximaDeBase altura
 
 longitudDeLaCopa :: Number -> Number
-longitudDeLaCopa altura = max 0 (altura - 3)
+longitudDeLaCopa altura = max 0 (altura - longitudMaximaDeBase)
 
 metrosACm :: Number -> Number
 metrosACm metros = metros * 100
@@ -31,7 +34,7 @@ costoTransporte :: Number -> Number
 costoTransporte altura
     | esLigero altura = 5000
     | esMediano altura = costoTransporteMediano altura
-    | esPesado altura = costoTransporteMediano altura + metrosACm altura
+    | esPesado altura = costoTransporteMediano altura + metrosACm altura --Aca se podria usar otherwise en vez de "esPesado altura"
 
 esLigero :: Number -> Bool
 esLigero altura = pesoPino altura < 500
@@ -44,4 +47,17 @@ esPesado altura = pesoPino altura > 800
 
 costoTransporteMediano :: Number -> Number
 costoTransporteMediano altura = 10 * pesoPino altura
+
+
+---- Alternativa de pesoPino con guardas:
+pesoPino2 :: Number -> Number
+pesoPino2 altura 
+    | altura <= longitudMaximaDeBase = pesoBase2 altura
+    | otherwise = pesoBase2 longitudMaximaDeBase + pesoCopa2 (altura - longitudMaximaDeBase)
+
+pesoBase2 :: Number -> Number
+pesoBase2 alturaBase = 3 * metrosACm alturaBase
+
+pesoCopa2 :: Number -> Number
+pesoCopa2 alturaCopa = 2 * metrosACm alturaCopa
 
